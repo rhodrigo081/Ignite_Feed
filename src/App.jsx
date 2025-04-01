dd7a98f4f25e458e3f9b0cb0e864bbf5ca63e8cc
rhodrigo081 <rhodrigo.leandro@gmail.com>
2025-03-31 21:49:14 -0300

    feat: adding features in the application

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,45 @@ import { Post } from "./components/Post.jsx";
 import "./global.css";
 import styles from "./App.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/rhodrigo081.png',
+      name: 'Rhodrigo Rodrigues',
+      role: 'Studant Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Olá! Meu nome é Rhodrigo e sou estudante do 2º período do tecnólogo em Análise e Desenvolvimento de Sistemas na FICR. Tenho paixão por tecnologia e desenvolvimento de software.Tenho conhecimento em Spring Boot, SQL e React, além de estar em constante aprendizado para me tornar um desenvolvedor mais completo. Atualmente, estou participando de trilhas de conhecimento e bootcamps para aprofundar minhas habilidades em desenvolvimento web e backend. Estou em busca de oportunidades para aplicar meus conhecimentos em projetos desafiadores e contribuir para soluções inovadoras.',
+      },
+      { type: 'link', content: 'rhodrigo081.developer/FICR' },
+    ],
+    publishedAt: new Date('2025-03-30 21:40:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/thxxx11.png',
+      name: 'João Lucas',
+      role: 'Studant Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-03-29 21:40:00'),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -12,8 +51,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,39 +1,57 @@
 import styles from "./Post.module.css";
 import { CommentArea } from "./CommentArea.jsx";
 import { Avatar } from "./Avatar.jsx";
+import { format, formatDistanceToNow } from 'date-fns';
+import { ptBR } from "date-fns/locale/pt-BR";
+import { useState } from "react";
 
-export function Post() {
+export function Post({ author, publishedAt,content }) {
+
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ])
+
+  const publishedDateFormate = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
+  function newComment(){
+    event.preventDefault()
+
+    setComments([...comments, comments.length + 1])
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar hasBorder={true} src="https://github.com/rhodrigo081.png" />
+          <Avatar hasBorder={true} src={author.avatarUrl} />
           <div className={styles.authorinfo}>
-            <strong>Rhodrigo Rodrigues</strong>
-            <span>Studant Development</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="02 de Fevereiro às 12:35" dateTime="2025-02-28 12:35:15">
-          Publicado há 2 dias
+        <time title={publishedDateFormate} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
         </time>
       </header>
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          👉 <a href="#">jane.design/doctorcare</a>{' '}
-        </p>
-        <p>
-          <a href="#">#novoprojeto</a>{' '}
-          <a href="#">#nlw</a>{' '}
-          <a href="#">#rocketseat</a>
-        </p>
+        {content.map(line => {
+          if(line.type === 'paragraph'){
+            return <p>{line.content}</p>
+          } else if(line.type === 'link'){
+            return <p><a href="">{line.content}</a></p>
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={newComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder="Deixe um comentário..." />
         <footer>
@@ -42,9 +60,9 @@ export function Post() {
       </form>
 
       <div className={styles.commentList}>
-        <CommentArea />
-        <CommentArea />
-        <CommentArea />
+        {comments.map(comment => {
+          return <CommentArea/>
+        })}
       </div>
     </article>
   );
